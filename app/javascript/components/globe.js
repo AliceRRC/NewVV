65aafee3a0dbf79d87c38ff3e9b24eb2a5e1d749
Alice Ratton <aliceratton@gmail.com>
2021-06-10 20:31:14 -0300

    styling fisrt page

--- app/javascript/components/globe.js
+++ app/javascript/components/globe.js
@@ -2,7 +2,7 @@
 
 
 function initializeGlobe() {
-    const options = { zoom: 2.0, position: [47.19537,8.524404] };
+    const options = { zoom: 1.6, position: [47.19537,8.524404] };
     const earth = new WE.map('earth_div', options);
     WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', {
           bounds: [[-85, -180], [85, 180]],
--- app/javascript/components/title.js
+++ app/javascript/components/title.js
@@ -3,10 +3,12 @@ import Typed from 'typed.js';
 
 function initTitle() {
  const typed = new Typed('#title', {
-  strings: ["V V", "VV", "V V" ],
-  typeSpeed: 100,
-  backSpeed: 100,
+  strings: ["V V", "VV", "V V", "VV", "V V", "VV", "V V", "VV", "V V", "VV", "V V"],
+  typeSpeed: 300,
+  backSpeed: 300,
   showCursor: false,
+  startDelay: 50,
+  backDelay: 50,
   loop: true
 });
 }
